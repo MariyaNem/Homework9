af89ea58f5721c290fcee91755e5c85bd539c015
Mariya Nemzorova <nemzorova777@mail.ru>
2023-02-05 23:41:33 +0700

    solved task 68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -12,7 +12,8 @@ Console.Write(result);
 
 int AckermannFunction(int m, int n)
 {
-    if (mNum == 0) return (n + 1);
-    else if (n == 0) return AckermannFunction(m - 1, 1);
-    else return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    if (m == 0) return (n + 1);
+    else if (m > 0 && n == 0) return AckermannFunction(m - 1, 1);
+    else if (m > 0 && n > 0) return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    return AckermannFunction(m,n);
 }
