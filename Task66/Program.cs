3398a0467bcd6b44f3960136fda54b4d89aebe3d
Mariya Nemzorova <nemzorova777@mail.ru>
2023-02-05 22:38:28 +0700

    Solve task 66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -3,3 +3,32 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.Write("Введите первое число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+if (min <= 0 || max <= 0)
+{
+    Console.Write("Введено число меньше или равное нулю");
+}
+else if (min > max)
+{
+    int temp = min;
+    min = max;
+    max = temp;
+    
+    int sumM = NaturalNumbersSum(min, max);
+    Console.Write(sumM);
+}
+else
+{
+    int sumM = NaturalNumbersSum(min, max);
+    Console.Write(sumM);
+}
+
+int NaturalNumbersSum(int minNum, int maxNum) //int sum=0
+{
+    if (minNum == maxNum) return minNum;
+    else return minNum + NaturalNumbersSum(minNum + 1, maxNum);
+}
