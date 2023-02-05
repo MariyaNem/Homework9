0e9c40c0793d403e65922461f80b969540908e3b
Mariya Nemzorova <nemzorova777@mail.ru>
2023-02-05 20:58:59 +0700

    Solve task 64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -3,3 +3,22 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0)
+{
+    Console.Write($"Введено не натуральное число");
+}
+else
+{
+    Console.Write($"N = {number} -> ");
+    NaturalNumbers(number);
+}
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+}
